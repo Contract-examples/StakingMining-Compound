221fe187880eca7d4f84791e0d0d56cc911a4423
gmh5225 <2315157@qq.com>
2024-11-04 16:44:03 +0800

    update

--- src/RNT.sol
+++ src/RNT.sol
@@ -3,16 +3,16 @@ pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-// TODO: EIP-2612
-contract RNT is ERC20, Ownable {
+contract RNT is ERC20, Ownable, ERC20Permit {
     // custom error
     error ExceedsMaxSupply(uint256 maxSupply, uint256 currentSupply, uint256 mintAmount);
 
     uint256 public constant MAX_SUPPLY = 1_000_000_000 * 10 ** 18; // max supply
     uint256 public immutable INITIAL_SUPPLY = 1_000_000 * 10 ** 18; // initial supply
 
-    constructor() ERC20("RNT", "RNT") Ownable(msg.sender) {
+    constructor() ERC20("RNT", "RNT") Ownable(msg.sender) ERC20Permit("RNT") {
         _mint(msg.sender, INITIAL_SUPPLY);
     }
 
